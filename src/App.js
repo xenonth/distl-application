6f64ae2dad7b95ae7d80fdeb7f0920de77a8025f
Timothy Forbes <timothy.forbes259@gmail.com>
2021-04-26 10:01:51 +0930

    updated styling for Why.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import React from "react";
 
 //import components for webpage into app in order of appearence
-import Header from "./components/Header";
-import PowerfulIntro from "./components/PowerfulIntro"
-import About from "./components/About"
-import Technology from "./components/Technology";
-import Why from "./components/Why";
-import TeamMember from "./components/TeamMember";
-import ContactUs from "./components/ContactUs";
-
-//import material UI Grid
-import Grid from "@material-ui/core/Grid"
+import MainPage from "./pages/MainPage"
 
 function App() {
   return (
-    <Grid container spacing ={1}>
-    
-    <Grid lg={1} ></Grid>
-
-      <Grid lg={10} md={10} xs={12}>
-        <Header />
-        <PowerfulIntro />
-        <About />
-        <Technology />
-        <Why />
-        <TeamMember />
-        <ContactUs />
-      </Grid>
-      
-    </Grid>
+    <MainPage />
   );
 }
 
--- src/components/Why.js
+++ src/components/Why.js
@@ -1,14 +1,63 @@
+/*Why section about Distl, Did think about creating a for loop for auto listing however this would require signicant change.
+Which would include creation of an array an additional sub component and having a data content approach and was decided to keep the format simple be leaving
+the provided text inside the html as is and only manipulate the css JS and some html elements as needed.
+
+*/ 
+
 import React from "react";
 
+import { makeStyles } from '@material-ui/core/styles';
+
+//Making Material UI stand out with the makestyles hook variant CSS Library
+const useStyles = makeStyles({
+    whyDISTL: {
+      color: "#F1FAEE",
+      background: "#E63946",
+  
+      //when the background is tapped on mobile or on hover the text and background will swap their colors
+      '&:hover': {
+          color: "#E63946",
+          background: "#F1FAEE"
+      },
+  
+      margin: "1%", 
+      paddingTop: "0.5em",
+      paddingBottom: "0.5em",
+    },
+    head: {
+      textAlign: "center"
+    },
+    list: {
+      color: "#F1FAEE",
+      background: "#E63946",  
+      //when the background is tapped on mobile or on hover the text and background will swap their colors
+      '&:hover': {
+          color: "#E63946",
+          background: "#F1FAEE"
+      },
+    }
+})
+
 function Why() {
+  const classes = useStyles();
     return (
-        <div class="why-distl">
-        <h4>Distl is an ideal place to cultivate your craft as a web developer.</h4>
-        <ul>
-          <li>You’ll be supported by a close-knit team of developers, and directly reporting to Distl’s technical lead.</li>
-          <li>You’ll work autonomously, both independently and collaboratively on projects creating websites and applications from scratch.</li>
-          <li>Projects constantly vary with lifecycles as short as four weeks, keeping your work fresh and challenging.</li>
-          <li>We'll gear you up with your very own Macbook Pro!</li>
+        <div className={classes.whyDISTL}>
+        <h4 className={classes.head}>
+          Distl is an ideal place to cultivate your craft as a web developer.
+          </h4>
+        <ul className={classes.list}>
+          <li>
+            You’ll be supported by a close-knit team of developers, and directly reporting to Distl’s technical lead.
+          </li>
+          <li>
+            You’ll work autonomously, both independently and collaboratively on projects creating websites and applications from scratch.
+          </li>
+          <li>
+            Projects constantly vary with lifecycles as short as four weeks, keeping your work fresh and challenging.
+          </li>
+          <li>
+            We'll gear you up with your very own Macbook Pro!
+          </li>
         </ul>
       </div>
     );
